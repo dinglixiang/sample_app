65448190ff2ccb2b7564366b0497f55991571302
丁历祥 <dlx_swpu@qq.com>
2013-03-06 21:30:22 +0800

    weibo

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,21 +4,23 @@ describe "UserPages" do
 
 	subject { page }
 
-  describe "edit" do
+  describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
-    before { visit edit_user_path(user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
 
-    describe "page" do
-      it { should have_selector('h1', text: "Update your profile") }
-      it { should have_selector('title', text: "Edit user") }
-      it { should have_link('change', href: 'http://gravatar.com/emails') }
-    end
+    before { visit user_path(user) }
 
-    describe "with invalid information" do
-      before { click_button "Save changes" }
-      it { should have_content('error') }
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
     end
   end
+
 
   describe "signup page" do
     before { visit signup_path }
