8891f18fd3afde876f3f9d0ae82224b939634bc1
丁历祥 <dlx_swpu@qq.com>
2013-03-06 20:40:01 +0800

    Finish user edit,update...

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,11 +27,10 @@ class User < ActiveRecord::Base
 
   before_save :create_remember_token
 
-
-
-end
-private
+  private
 
     def create_remember_token
       self.remember_token = SecureRandom.urlsafe_base64
     end
+
+end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    name     "dinglixiang"
-    email    "dlx_swpu@qq.com"
-    password "ding1991"
-    password_confirmation "ding1991"
+    sequence(:name) { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
   end
 end
